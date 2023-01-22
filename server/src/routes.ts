70432f6f1506deb3bc13369482022561ec956b08
Fernando Varella Lopes <fernando.varella@totvs.com.br>
2023-01-22 00:27:46 -0300

    Projetos finalizados

---
+++ server/src/routes.ts
@@ -0,0 +1,149 @@
+import { FastifyInstance } from "fastify"
+import { prisma } from "./lib/prisma"
+import { z } from 'zod'
+import dayjs from "dayjs"
+
+export async function appRoutes(app: FastifyInstance) {
+    app.post('/habits', async (request) => {
+        const createHabitBody = z.object({
+            title: z.string(),
+            weekDays: z.array(z.number().min(0).max(6))
+        })
+
+
+        const { title, weekDays } = createHabitBody.parse(request.body);
+
+        const today = dayjs().startOf('day').toDate();
+
+        await prisma.habit.create({
+            data: {
+                title,
+                created_at: today,
+                weekDays: {
+                    create: weekDays.map(weekDay => {
+                        return { week_day: weekDay }
+                    })
+                }
+            }
+        })
+    })
+
+    app.get('/day', async (request) => {
+        const getDayParams = z.object({
+            date: z.coerce.date()
+        })
+
+        const { date } = getDayParams.parse(request.query);
+        const weekDay = dayjs(date).get('day')
+
+        const possibleHabits = await prisma.habit.findMany({
+            where: {
+                created_at: {
+                    lte: date
+                },
+                weekDays: {
+                    some: {
+                        week_day: weekDay
+                    }
+                }
+            }
+        })
+
+        const day = await prisma.day.findUnique({
+            where: {
+                date: date
+            }, include: {
+                dayHabits: true
+            }
+        })
+
+        const completedHabits = day?.dayHabits.map(dayHabit => {
+            return dayHabit.habit_id
+        }) ?? [];
+
+        return {
+            possibleHabits, completedHabits
+        }
+    })
+
+    app.patch('/habits/:id/toggle', async (request) => {
+        const togleHabitParams = z.object({
+            id: z.string().uuid()
+        })
+        const { id } = togleHabitParams.parse(request.params);
+        const today = dayjs().startOf('day').toDate();
+
+        let day = await prisma.day.findUnique({
+            where: {
+                date: today
+            }
+        })
+
+        if (!day) {
+            day = await prisma.day.create({
+                data: {
+                    date: today
+                }
+            })
+        }
+
+        const dayHabit = await prisma.dayHabit.findUnique({
+            where: {
+                day_id_habit_id: {
+                    day_id: day.id,
+                    habit_id: id
+                }
+            }
+        })
+
+        if (dayHabit) {
+            await prisma.dayHabit.delete({
+                where: {
+                    id: dayHabit.id
+                }
+            })
+        } else {
+
+            await prisma.dayHabit.create({
+                data: {
+                    day_id: day.id,
+                    habit_id: id
+                }
+            })
+        }
+
+    })
+
+    app.get('/summary', async () => {
+
+
+
+        const summary = await prisma.$queryRaw`
+            select 
+                d.id, 
+                d.date,
+                (
+                    select 
+                        cast(count(*) as float)
+                    from day_habits dh
+                    where dh.day_id = d.id
+                ) as completed,
+                (
+                    select  
+                        cast(count(*) as float)
+                    from habit_week_days hwd
+                    join habits h
+                        on h.id = hwd.habit_id
+                    where 
+                        hwd.week_day = cast(strftime('%w', d.date/1000.0, 'unixepoch') as int)
+                        and h.created_at <= d.date
+                ) as amount
+            from days d
+          `
+
+        return summary
+    })
+
+
+}
+
---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import {PrismaClient} from '@prisma/client'
+import { prisma } from './lib/prisma'
+import { appRoutes } from './routes'
+
+const app = Fastify()
+
+app.register(cors)
+app.register(appRoutes)
+
+app.listen({
+    port: 3333,
+    host: '0.0.0.0'
+}).then(()=>{
+    console.log('http Server running')
+})
